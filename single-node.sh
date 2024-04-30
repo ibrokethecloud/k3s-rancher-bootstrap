f6a6a26cae2b13a2632bb3d33eb304c8a9c0af18
Gaurav Mehta <gaurav.mehta@suse.com>
2024-04-30 18:33:26 +1000

    updated versions

--- single-node.sh
+++ single-node.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Installing K3S"
-curl  -sfL https://get.k3s.io  | sh -
+curl  -sfL https://get.k3s.io  | INSTALL_K3S_CHANNEL="v1.27" sh -
 
 echo "Downlading cert-manager CRDs"
-wget -q -P /var/lib/rancher/k3s/server/manifests/ https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.crds.yaml
+wget -q -P /var/lib/rancher/k3s/server/manifests/ https://github.com/jetstack/cert-manager/releases/download/v1.5.1/cert-manager.crds.yaml
 
 cat > /var/lib/rancher/k3s/server/manifests/rancher.yaml << EOF
 apiVersion: v1
@@ -27,7 +27,7 @@ spec:
   targetNamespace: cert-manager
   repo: https://charts.jetstack.io
   chart: cert-manager
-  version: v0.15.0
+  version: v1.5.1
   helmVersion: v3
 ---
 apiVersion: helm.cattle.io/v1
@@ -39,10 +39,13 @@ spec:
   targetNamespace: cattle-system
   repo: https://releases.rancher.com/server-charts/stable/
   chart: rancher
+  version: v2.8.1
   set:
-    hostname: k3s-one
     ingress.tls.source: rancher
+    hostname: rancher.ibrokethe.cloud
     replicas: 1
+    global.cattle.psp.enabled: "false"
+    bootstrapPassword: password1234
   helmVersion: v3
 EOF
 
